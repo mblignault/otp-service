c29f110d07f33d4f68e7a22b0bf2af5a8a83a883
Test User <test@michael.entrostat.dev>
2023-12-01 07:03:23 +0000

    Frontend logic and basic styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,90 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Js connected");
     const requestForm = document.getElementById("request-form");
+    const validateForm = document.getElementById("validation-form");
 
-    requestForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const email = document.getElementById("email").value;
-        console.log("Submit button clicked", email);
-        requestOTP(email);
-    });
+    if (requestForm) {
+        requestForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const email = document.getElementById("email").value;
+
+            const submitType = e.submitter.value;
+            if (submitType === "resend") {
+                console.log("Resend button clicked", email);
+            } else {
+                console.log("Submit button clicked", email);
+                requestOTP(email);
+            }
+        });
+    }
+
+    if (validateForm) {
+        validateForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const email = document.getElementById("validation-email").value;
+            const otp = document.getElementById("validation-otp").value;
+            console.log("Submit button clicked", email, otp);
+            validateOTP(email, otp);
+        });
+    }
 });
 
 function requestOTP(email) {
+    notificationDiv = document.getElementById("generate-notification");
+    notificationDiv.textContent = "";
+    notificationDiv.classList.remove("error-message");
+    notificationDiv.classList.remove("success-message");
     fetch(`api/otp/generate?email=${email}`, {
         method: "POST",
         headers: {
             "Content-type": "application/json",
         },
-        // body: JSON.stringify({ email: email }),
     })
         .then((response) => response.json())
-        .then((data) => console.log("Generate Data: ", data))
-        .catch((err) => console.error("Error: ", err));
+        .then((data) => {
+            console.log("Generate Data: ", data);
+            if (data.success === false) {
+                notificationDiv.classList.add("error-message");
+            } else {
+                notificationDiv.classList.add("success-message");
+            }
+            notificationDiv.textContent = data.message;
+        })
+        .catch((err) => {
+            console.error("Error: ", err);
+            notificationDiv.classList.add("error-message");
+            notificationDiv.textContent = "Error generating OTP.";
+        });
+}
+
+function resendOTP(email) {
+    console.log("Resending");
 }
 
 function validateOTP(email, otp) {
+    notificationDiv = document.getElementById("validate-notification");
+    notificationDiv.textContent = "";
+    notificationDiv.classList.remove("error-message");
+    notificationDiv.classList.remove("success-message");
     fetch(`api/otp/validate?email=${email}&otp=${otp}`, {
-        method: "POST",
+        method: "PUT",
         headers: {
             "Content-type": "application/json",
         },
-        // body: JSON.stringify({ email: email, otp: otp }),
     })
         .then((response) => response.json())
-        .then((data) => console.log("Validate Data: ", data))
-        .catch((err) => console.error("Error: ", err));
+        .then((data) => {
+            console.log("Validate Data: ", data);
+            if (data.success === false) {
+                notificationDiv.classList.add("error-message");
+            } else {
+                notificationDiv.classList.add("success-message");
+            }
+            notificationDiv.textContent = data.message;
+        })
+        .catch((err) => {
+            console.error("Error: ", err);
+            notificationDiv.classList.add("error-message");
+            notificationDiv.textContent = "Error validating OTP.";
+        });
 }
